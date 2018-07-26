38b26dfe2a87673d7dfbc882fccc645949a46bea
Admin <Admin@Triconws016.triconhome.local>
2018-07-26 18:14:01 +0530

    my maven project

---
+++ ashmi/src/main/java/com/tricon/app/App.java
@@ -0,0 +1,22 @@
+package com.tricon.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+		int i=0,j=0;
+		for(i=5;i>=1;i--)
+		{
+			for(j=0;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+}
